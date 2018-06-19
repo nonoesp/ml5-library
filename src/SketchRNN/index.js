f8bd6e865c8d737b43d0c98efc081397611f39fd
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-06-18 20:47:44 -0400

    🏗 Add SketchRNN scaffold.

---
+++ src/SketchRNN/index.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* eslint max-len: "off" */
+/*
+SketchRNN
+*/
+
+class SketchRNN {
+    constructor() {
+
+    }
+}
+
+const sketchRNN = () => {
+    return new SketchRNN();
+};
+
+export default sketchRNN;
